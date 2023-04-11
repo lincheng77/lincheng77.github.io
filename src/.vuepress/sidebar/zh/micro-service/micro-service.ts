650081415866d9ac46b04efef3460d3ab71ec673
“dingmd” <dingmd@cffex.com.cn>
2023-04-11 15:27:14 +0800

    【Nacos2.2版本Tomcat启动报错.md 】修改目录 【micro-service.ts 】增加注释

--- src/.vuepress/sidebar/zh/micro-service/micro-service.ts
+++ src/.vuepress/sidebar/zh/micro-service/micro-service.ts
@@ -6,6 +6,6 @@ import {nacos} from "./nacos/nacos"
 
 
 export const microService: SidebarOptions ={
-        "/micro-service/nacos/": nacos,
-        // "/micro-service/nacos/error": error,
+        "/micro-service/nacos/": nacos, //在当前目录配置的erroe中的内容会嵌套在当前sidebar
+        // "/micro-service/nacos/error": error, //error配置的erroe中的内容会开启一个新的sidebar
 };
