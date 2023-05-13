458992576c3dd2887075e31004f70bea98ba0a0b
lincheng <1498047434@qq.com>
2023-05-13 12:23:55 +0800

    进一步研究侧边栏

--- src/.vuepress/sidebar/zh/micro-service/micro-service.ts
+++ src/.vuepress/sidebar/zh/micro-service/micro-service.ts
@@ -1,11 +1,11 @@
 import {sidebar, SidebarOptions} from "vuepress-theme-hope";
 import {nacos} from "./nacos/nacos"
-// import {error} from "./nacos/error"
+//import {error} from "./nacos/error"
 
 
 
 
 export const microService: SidebarOptions ={
         "/micro-service/nacos/": nacos, //在当前目录配置的erroe中的内容会嵌套在当前sidebar
-        // "/micro-service/nacos/error": error, //error配置的erroe中的内容会开启一个新的sidebar
+        //         "/micro-service/nacos/error": error, //error配置的error中的内容会开启一个新的sidebar,不设置就会嵌套在nacos中
 };
--- src/.vuepress/sidebar/zh/micro-service/nacos/nacos.ts
+++ src/.vuepress/sidebar/zh/micro-service/nacos/nacos.ts
@@ -4,15 +4,38 @@ import { arraySidebar } from "vuepress-theme-hope";
 export const nacos = arraySidebar([
     "",
     {
-        text: "遇坑报错",
+        text: "安装部署",
         icon: "module",
+        collapsible: true,  // 可选的, 设置分组是否可以折叠，默认值是 false,
+        children: [
+            {
+                text: "Nacos单机集群部署搭建",
+                link: "Nacos单机集群部署搭建"
+            },
+            {
+                text: "Nacos单机集群部署搭建1",
+                link: "Nacos单机集群部署搭建1"
+            },
+        ]
+    },
+    {
+        text: "错误总结",
+        icon: "module",
+        collapsible: true,
+
+        //link: "/micro-service/nacos/error",// 可选的, 分组标题(错误总结)对应的链接
+
+        prefix: "error/",// 可选的，会添加到每个children item 链接地址之前
+
         children: [
             {
                 text: "Nacos2.2版本Tomcat启动报错",
-                link: "error/Nacos2.2版本Tomcat启动报错"
+                link: "Nacos2.2版本Tomcat启动报错"
             },
-            "windows/",
-             "github/"
+            {
+                text: "Nacos2.2版本Tomcat启动报错",
+                link: "Nacos2.2版本Tomcat启动报错"
+            },
         ]
     },
     {
