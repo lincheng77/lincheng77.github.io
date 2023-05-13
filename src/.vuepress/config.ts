b2ba07982ed70eb5398257e5b75457f54c05efb0
lincheng <1498047434@qq.com>
2023-05-13 17:35:52 +0800

    增加评论插件，配置评论插件Giscus

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { commentPlugin } from "vuepress-plugin-comment2";
 
 export default defineUserConfig({
 
@@ -25,4 +26,16 @@ export default defineUserConfig({
 
   // Enable it with pwa
   // shouldPrefetch: false,
+
+  plugins: [
+    commentPlugin({
+      // 插件选项
+      provider: "Giscus", //评论服务提供者。
+      comment: true, //启用评论功能
+      repo: "lincheng77/giscus-repo", //远程仓库
+      repoId: "MDEwOlJlcG9zaXRvcnkzNjg1OTk3NDM=", //对应自己的仓库Id
+      category: "General",
+      categoryId: "DIC_kwDOFfhiv84CWdWk" //对应自己的分类Id
+    }),
+    ],
 });
